1286c365e1ce15aaaffeb59f304bd2e80dbc824e
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2024-10-22 11:45:14 +0530

    Add git branch

---
+++ branch.py
@@ -0,0 +1,11 @@
+def git_branch():
+    '''
+    Git branch is used to create a new branch.
+    Commands used are : `git branch <branch name>`
+                        `git checkout -b <branch name>`
+    
+    Note: 1. When you use git branch , it just creates a branch.
+             So, you have to use `git switch` or `git checkout <branch name>` to go to that branch.
+          2. But, When you use `git checkout -b`, it will automatically switched to the new branch.
+    '''
+    pass
